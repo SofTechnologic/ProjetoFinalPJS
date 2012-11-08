6177e8ce7763e6d5c30098de39bc05a0f9846284
Felipe Cherubin <felipe_cherubin@hotmail.com>
2012-11-08 19:34:30 -0200

    colocando campos nulos e nao nulos no banco de dados

--- SysMusicCollection/frmCadAmigo.Designer.cs
+++ SysMusicCollection/frmCadAmigo.Designer.cs
@@ -49,20 +49,20 @@
             // 
             this.tbpCadastro.Location = new System.Drawing.Point(4, 22);
             this.tbpCadastro.Name = "tbpCadastro";
-            this.tbpCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpCadastro.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.tbpCadastro.Size = new System.Drawing.Size(688, 394);
             this.tbpCadastro.TabIndex = 0;
-            this.tbpCadastro.Text = "tabPage1";
+            this.tbpCadastro.Text = "Discos";
             this.tbpCadastro.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(688, 394);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Amigos";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // frmCadastro
@@ -73,7 +73,7 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "frmCadastro";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmCadAmigo";
+            this.Text = "Cadastro de Discos/Amigos";
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
