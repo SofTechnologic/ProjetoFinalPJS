6109e7f4a0be1cda67c3e82530bb924b69cb3817
Thiago Carvalho <thiagocarv@uol.com.br>
2012-11-08 22:30:15 -0200

    criacao da class de conexao

---
+++ SysMusicCollection/conexaoBanco.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SysMusicCollection
+{
+    public class conexaoBanco
+    {
+        SqlConnection cnx = new SqlConnection();
+
+        //static void Main(string[] args)
+        //{
+            
+            
+        //}
+            public void abrir()
+            {
+                cnx.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\sysMusicColletion\BD\dbSysMusicColletion.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+                try
+                {
+                    cnx.Open();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            public void fechar()
+
+            {
+                if (cnx != null)
+                    cnx.Close();
+            }
+        }
+    }
+
